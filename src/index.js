76f1cb89bbc6e7517bc3a0693fbf2b5f9562beb0
Armand135 <kokov.azamat@gmail.com>
2025-06-21 22:48:06 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,20 +7,35 @@ import ScanPage from "./pages/ScanPage";
 import ResultPage from "./pages/ResultPage";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
-
+import PrivateRoute from "./components/PrivateRoute";
 import "./style.css";
 
-const App = () => (
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <Router>
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/scan" element={<ScanPage />} />
-      <Route path="/result" element={<ResultPage />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
+
+      {/* Protected routes */}
+      <Route
+        path="/scan"
+        element={
+          <PrivateRoute>
+            <ScanPage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/result"
+        element={
+          <PrivateRoute>
+            <ResultPage />
+          </PrivateRoute>
+        }
+      />
     </Routes>
   </Router>
 );
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
