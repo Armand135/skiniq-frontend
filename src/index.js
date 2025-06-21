711bc5201afc8660641140b35b3db80957cee549
Armand135 <kokov.azamat@gmail.com>
2025-06-21 22:41:22 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import HomePage from "./pages/HomePage";
 import ScanPage from "./pages/ScanPage";
 import ResultPage from "./pages/ResultPage";
+import Signup from "./pages/Signup";
+
 import "./style.css";
 
-ReactDOM.render(
+const App = () => (
   <Router>
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/scan" element={<ScanPage />} />
       <Route path="/result" element={<ResultPage />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
-  </Router>,
-  document.getElementById("root")
+  </Router>
 );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
