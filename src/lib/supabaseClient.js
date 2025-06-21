9eafc2ea5d1cca39fbaa39535cc233932263519c
Armand135 <kokov.azamat@gmail.com>
2025-06-21 22:27:27 +0400

    Update supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,5 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://kkhlrtxvtojjuypfjizr.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtraGxydHh2dG9qanV5cGZqaXpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA1MjEzMTMsImV4cCI6MjA2NjA5NzMxM30.aAtE5AJr1Fp_Jy5vRxeJHTurxSSR-40FXt9dEdju0rI';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabaseUrl = 'https://kkhlrtxvtojjuypfjizr.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
