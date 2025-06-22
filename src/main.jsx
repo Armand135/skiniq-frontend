9c0b28b5f8bdbec7666b4b422835094b34aeeff4
Armand135 <kokov.azamat@gmail.com>
2025-06-22 20:44:10 +0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import HomePage from "./pages/HomePage";
+import ScanPage from "./pages/ScanPage";
+import ResultPage from "./pages/ResultPage";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import History from "./pages/History";
+import PrivateRoute from "./components/PrivateRoute";
+import "./style.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/history" element={<PrivateRoute><History /></PrivateRoute>} />
+        <Route path="/scan" element={<PrivateRoute><ScanPage /></PrivateRoute>} />
+        <Route path="/result" element={<PrivateRoute><ResultPage /></PrivateRoute>} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
